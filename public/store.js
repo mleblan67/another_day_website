4643ad04562d22d79b84f470a124e9118e30c1b9
mleblan67 <mleblan67@charlottesvilleschools.org>
2020-06-26 23:16:05 -0400

    make code prettier

--- public/store.js
+++ public/store.js
@@ -1,141 +1,160 @@
 //get product info
-var itemInfo = localStorage['itemInfo'];//[id,color,size,price,product]
-itemInfo = itemInfo.split(',')
-var customerInfo = []
-var shipping
-var zipcode
+var itemInfo = localStorage["itemInfo"]; //[id,color,size,price,product]
+itemInfo = itemInfo.split(",");
+var customerInfo = [];
+var shipping;
+var zipcode;
 
 //turn into JSON
 itemInfo = {
-    id:itemInfo[0],
-    color:itemInfo[1],
-    size:itemInfo[2],
-    price:itemInfo[3],
-    product:itemInfo[4],
-    shipping:[itemInfo[5],itemInfo[6],itemInfo[7]]
+  id: itemInfo[0],
+  color: itemInfo[1],
+  size: itemInfo[2],
+  price: itemInfo[3],
+  product: itemInfo[4],
+  shipping: [itemInfo[5], itemInfo[6], itemInfo[7]],
+};
 
-}
-
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
 } else {
-    ready()
+  ready();
 }
 
-function calculate_shipping(){
-    zip_code = document.getElementById('zip').value
-    shipping = "Enter ZIP code"
-    //make sure it's a valid zip
-    if(zip_code > 10000){
-        if(zip_code>10000 && zip_code<40000){
-            shipping = itemInfo.shipping[0]/100
-        }else if(zip_code > 40000 && zip_code<80000){
-            shipping =itemInfo.shipping[1]/100
-        }else if(zip_code > 80000 && zip_code<100000){
-            shipping =itemInfo.shipping[2]/100
-        }
-        //update shipping price and total price
-        document.getElementById('order_shipping').innerHTML= '$'+shipping
-        document.getElementById('order_total').innerHTML = '$'+(itemInfo.price/100+shipping)
-    }else{
-        document.getElementById('order_shipping').innerHTML= "Enter ZIP code"
-        document.getElementById('order_total').innerHTML = ""
+function calculate_shipping() {
+  zip_code = document.getElementById("zip").value;
+  shipping = "Enter ZIP code";
+  //make sure it's a valid zip
+  if (zip_code > 10000) {
+    if (zip_code > 10000 && zip_code < 40000) {
+      shipping = itemInfo.shipping[0] / 100;
+    } else if (zip_code > 40000 && zip_code < 80000) {
+      shipping = itemInfo.shipping[1] / 100;
+    } else if (zip_code > 80000 && zip_code < 100000) {
+      shipping = itemInfo.shipping[2] / 100;
     }
+    //update shipping price and total price
+    document.getElementById("order_shipping").innerHTML = "$" + shipping;
+    document.getElementById("order_total").innerHTML =
+      "$" + (itemInfo.price / 100 + shipping);
+  } else {
+    document.getElementById("order_shipping").innerHTML = "Enter ZIP code";
+    document.getElementById("order_total").innerHTML = "";
+  }
 }
-
 
 function ready() {
-    //display order summary
-    let order_product = document.getElementById('order_summary')
-    let order_price = document.getElementById('order_summary_price')
+  //display order summary
+  let order_product = document.getElementById("order_summary");
+  let order_price = document.getElementById("order_summary_price");
 
-    order_product.innerHTML = '<strong>'+ itemInfo.product + '<br>'+ 'Color: '+ itemInfo.color.replace("_"," ") + '<br>' + 'Size: ' + itemInfo.size.toUpperCase() + '</strong>';
-    order_price.innerHTML = '$'+itemInfo.price/100
+  order_product.innerHTML =
+    "<strong>" +
+    itemInfo.product +
+    "<br>" +
+    "Color: " +
+    itemInfo.color.replace("_", " ") +
+    "<br>" +
+    "Size: " +
+    itemInfo.size.toUpperCase() +
+    "</strong>";
+  order_price.innerHTML = "$" + itemInfo.price / 100;
 
-    //calculate shipping based on zip
-    for(let i =0;i<document.getElementsByClassName('info-input').length; i++){
-        document.getElementsByClassName('info-input')[i].addEventListener('focusout',calculate_shipping)
-    }
+  //calculate shipping based on zip
+  for (
+    let i = 0;
+    i < document.getElementsByClassName("info-input").length;
+    i++
+  ) {
+    document
+      .getElementsByClassName("info-input")
+      [i].addEventListener("focusout", calculate_shipping);
+  }
 
-
-    document.getElementById('confirm_button').addEventListener('click', function(){
-        //Check to make sure all info is filled in
-        inputs=document.getElementsByClassName("info-input")
-        var filled_counter = 0;
-        for(var i = 0; i<inputs.length; i++){
-            if(inputs[i].value==""){
-                inputs[i].style.border = "3px solid red"
-                filled_counter++
-            } else{
-                inputs[i].style.border = "3px solid #3c8ccc"
-            }
+  document
+    .getElementById("confirm_button")
+    .addEventListener("click", function () {
+      //Check to make sure all info is filled in
+      inputs = document.getElementsByClassName("info-input");
+      var filled_counter = 0;
+      for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == "") {
+          inputs[i].style.border = "3px solid red";
+          filled_counter++;
+        } else {
+          inputs[i].style.border = "3px solid #3c8ccc";
         }
-        if(filled_counter>0){
-            console.log("not complete")
-        } else{
-            //put together all customer contact info into JSON format
-            info_inputs = document.getElementsByClassName('info-input')
-            for(let i = 0; i<info_inputs.length; i++){
-                customerInfo[info_inputs[i].name] = info_inputs[i].value
-            }
-            customerInfo = $('#information-container').serializeArray()
-            console.log(customerInfo)
-            console.log(itemInfo)
-            //go to stripe checkout
-            let total = parseInt(itemInfo.price)+(shipping*100)
-            stripeHandler.open({amount:total});
+      }
+      if (filled_counter > 0) {
+        console.log("not complete");
+      } else {
+        //put together all customer contact info into JSON format
+        info_inputs = document.getElementsByClassName("info-input");
+        for (let i = 0; i < info_inputs.length; i++) {
+          customerInfo[info_inputs[i].name] = info_inputs[i].value;
         }
-    })
+        customerInfo = $("#information-container").serializeArray();
+        console.log(customerInfo);
+        console.log(itemInfo);
+        //go to stripe checkout
+        let total = parseInt(itemInfo.price) + shipping * 100;
+        stripeHandler.open({ amount: total });
+      }
+    });
 }
 
-console.log("Key = "+ stripePublicKey)
+console.log("Key = " + stripePublicKey);
 
 var stripeHandler = StripeCheckout.configure({
-    key: stripePublicKey,
-    locale: 'en',
-    token: function(token) {
-        item = itemInfo.id
+  key: stripePublicKey,
+  locale: "en",
+  token: function (token) {
+    item = itemInfo.id;
 
-        fetch('/purchase', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                stripeTokenId: token.id,
-                items: item,
-                zip:zip_code
-            })
-        }).then(function(res) {
-            console.log(res.json())
-        }).then(function(data) {
-            //send order info to server for Google spreadsheet
-            fetch('/send_order',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: JSON.stringify({
-                    item_information:itemInfo,
-                    customer_information:customerInfo
-                })
-            }).then(function(res) {
-                console.log(res.json())
-            }).then(function(data) {
-                console.log(data)
-            })
-
-            //clear local storage
-            localStorage.removeItem('itemInfo'); // Clear the localStorage
-            localStorage.removeItem('itemColor')
-            localStorage.removeItem('itemId')
-            //go to success screen
-            window.location.href = 'success.html'
-        }).catch(function(error) {
-            console.error(error)
+    fetch("/purchase", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        stripeTokenId: token.id,
+        items: item,
+        zip: zip_code,
+      }),
+    })
+      .then(function (res) {
+        console.log(res.json());
+      })
+      .then(function (data) {
+        //send order info to server for Google spreadsheet
+        fetch("/send_order", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            item_information: itemInfo,
+            customer_information: customerInfo,
+          }),
         })
-    }
-})
+          .then(function (res) {
+            console.log(res.json());
+          })
+          .then(function (data) {
+            console.log(data);
+          });
 
+        //clear local storage
+        localStorage.removeItem("itemInfo"); // Clear the localStorage
+        localStorage.removeItem("itemColor");
+        localStorage.removeItem("itemId");
+        //go to success screen
+        window.location.href = "success.html";
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  },
+});
--- server.js
+++ server.js
@@ -1,184 +1,186 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
 }
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 //reading JSON
-const fs = require('fs')
+const fs = require("fs");
 //MongoDB for inventory management
-var mongoClient = require('mongodb').MongoClient
+var mongoClient = require("mongodb").MongoClient;
 //fill out url
-var url = "mongodb+srv://{username}:{password}@another-day-cluster-lhylq.mongodb.net/another-day-cluster?retryWrites=true&w=majority"
-url = url.replace("{username}", process.env.MONGO_USERNAME)
-url = url.replace("{password}", process.env.MONGO_PASSWORD)
+var url = "mongodb+srv://{username}:{password}@another-day-cluster-lhylq.mongodb.net/another-day-cluster?retryWrites=true&w=majority";
+url = url.replace("{username}", process.env.MONGO_USERNAME);
+url = url.replace("{password}", process.env.MONGO_PASSWORD);
 //Stripe charging API
-const stripe = require('stripe')(stripeSecretKey)
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const stripe = require("stripe")(stripeSecretKey);
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 // set up google spreadsheet credentials
-const doc = new GoogleSpreadsheet('1doVU69EV3zOfJdCvR5eRMUdWW_xfBCvUjr72wdMrfR0');
+const doc = new GoogleSpreadsheet(
+  "1doVU69EV3zOfJdCvR5eRMUdWW_xfBCvUjr72wdMrfR0"
+);
 var accountInfo = {
-  "client_email": process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  "private_key": process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-}
+  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 // use service account creds
 doc.useServiceAccountAuth(accountInfo);
 
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static("public"));
 
-
-function update_quantities(itemId){
-  console.log(itemId)
+function update_quantities(itemId) {
+  console.log(itemId);
   //connect to Mongo database
-  mongoClient.connect(url, function(err,db) {
-    if (err) throw err
-    var dbo = db.db("another-day-cluster")
-    var query = {id:itemId}
+  mongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("another-day-cluster");
+    var query = { id: itemId };
     //find right query
-    dbo.collection("quantities").find(query).toArray(function(err, result) {
-      if (err) throw err;
-      var new_quantity = {$set:{quantity: (result[0].quantity-1)}}
-      //update item quantity
-      dbo.collection("quantities").updateOne(query, new_quantity, function(err, res) {
+    dbo
+      .collection("quantities")
+      .find(query)
+      .toArray(function (err, result) {
         if (err) throw err;
-        console.log("1 document updated");
-        db.close();
+        var new_quantity = { $set: { quantity: result[0].quantity - 1 } };
+        //update item quantity
+        dbo
+          .collection("quantities")
+          .updateOne(query, new_quantity, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+            db.close();
+          });
       });
-    });
-  })
+  });
 }
 
-async function send_order(dateTime,order_info,customer_info){
+async function send_order(dateTime, order_info, customer_info) {
   //append customer info to google spreadsheet
-  await doc.loadInfo()
+  await doc.loadInfo();
   const sheet = doc.sheetsByIndex[0];
   sheet.addRow({
     date: dateTime,
-    name:customer_info.fname + " " + customer_info.lname,
-    email:customer_info.email,
-    product:order_info.product,
-    color:order_info.color,
-    size:order_info.size,
-    address:customer_info.address,
-    city:customer_info.city,
-    state:customer_info.state,
-    zip:customer_info.zip
-  })
+    name: customer_info.fname + " " + customer_info.lname,
+    email: customer_info.email,
+    product: order_info.product,
+    color: order_info.color,
+    size: order_info.size,
+    address: customer_info.address,
+    city: customer_info.city,
+    state: customer_info.state,
+    zip: customer_info.zip,
+  });
 }
-
 //send item info over to product page
-app.get('/get_info', function(req,res) {
+app.get("/get_info", function (req, res) {
   //load in how much is left of each item from the Mongo DB
-  mongoClient.connect(url, function(err,db){
-    if (err) throw err
-    var dbo = db.db("another-day-cluster")
-    dbo.collection("quantities").find({}).toArray(function(err, result) {
-      if (err) throw err;
-      //load item data
-      fs.readFile('items.json', function(error, data) {
-        if (error) {
-          res.status(500).end()
-        } else {
-          //send item and quantity data to product page
-          res.send({
-            items:JSON.parse(data),
-            quantity:result
-          })
-        }
-      })
-      db.close();
-    });
-  })
-
-  
-
-})
-
-
-app.get('/confirm', function(req, res) {
-  fs.readFile('items.json', function(error, data) {
+  mongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("another-day-cluster");
+    dbo
+      .collection("quantities")
+      .find({})
+      .toArray(function (err, result) {
+        if (err) throw err;
+        //load item data
+        fs.readFile("items.json", function (error, data) {
+          if (error) {
+            res.status(500).end();
+          } else {
+            //send item and quantity data to product page
+            res.send({
+              items: JSON.parse(data),
+              quantity: result,
+            });
+          }
+        });
+        db.close();
+      });
+  });
+});
+app.get("/confirm", function (req, res) {
+  fs.readFile("items.json", function (error, data) {
     if (error) {
-      res.status(500).end()
+      res.status(500).end();
     } else {
-      res.render('confirm.ejs', {
+      res.render("confirm.ejs", {
         stripePublicKey: stripePublicKey,
-        items: JSON.parse(data)
-      })
+        items: JSON.parse(data),
+      });
     }
-  })
-})
+  });
+});
 
-app.post('/purchase', function(req, res) {
-  fs.readFile('items.json', function(error, data) {
+app.post("/purchase", function (req, res) {
+  fs.readFile("items.json", function (error, data) {
     if (error) {
-      console.log("error reading JSON")
-      res.status(500).end()
+      console.log("error reading JSON");
+      res.status(500).end();
     } else {
-      console.log("able to read JSON")
-      
-      const itemsJson = JSON.parse(data)
-      const itemsArray = itemsJson.clothing
-      let total = 0
-      itemId = req.body.items
-      console.log(itemId)
-      const itemJson = itemsArray.find(function(i) {
-        return i.id == itemId
-      })
+      console.log("able to read JSON");
+
+      const itemsJson = JSON.parse(data);
+      const itemsArray = itemsJson.clothing;
+      let total = 0;
+      itemId = req.body.items;
+      console.log(itemId);
+      const itemJson = itemsArray.find(function (i) {
+        return i.id == itemId;
+      });
       //calculate shipping
-      let shipping = 0
-      let zip_code = req.body.zip
+      let shipping = 0;
+      let zip_code = req.body.zip;
 
-      if(zip_code>10000 && zip_code<40000){
-        shipping = itemJson.shipping[0]
-      }else if(zip_code > 40000 && zip_code<80000){
-          shipping =itemJson.shipping[1]
-      }else if(zip_code > 80000 && zip_code<100000){
-          shipping =itemJson.shipping[2]
+      if (zip_code > 10000 && zip_code < 40000) {
+        shipping = itemJson.shipping[0];
+      } else if (zip_code > 40000 && zip_code < 80000) {
+        shipping = itemJson.shipping[1];
+      } else if (zip_code > 80000 && zip_code < 100000) {
+        shipping = itemJson.shipping[2];
       }
-      
-      total = itemJson.price + shipping
-      console.log(total)
-      console.log(req.body.stripeTokenId)
-      
-      stripe.charges.create({
-        amount: total,
-        source: req.body.stripeTokenId,
-        currency: 'usd'
-      }).then(function() {
-        console.log('Charge Successful')
-        res.json({ message: 'Successfully purchased items' })
-      }).catch(function(error) {
-        console.log('Charge Fail')
-        console.log(error)
-        res.status(500).end()
-      })
-      
+
+      total = itemJson.price + shipping;
+      console.log(total);
+      console.log(req.body.stripeTokenId);
+
+      stripe.charges
+        .create({
+          amount: total,
+          source: req.body.stripeTokenId,
+          currency: "usd",
+        })
+        .then(function () {
+          console.log("Charge Successful");
+          res.json({ message: "Successfully purchased items" });
+        })
+        .catch(function (error) {
+          console.log("Charge Fail");
+          console.log(error);
+          res.status(500).end();
+        });
     }
-  })
+  });
+});
 
-})
-
-
-app.post('/send_order', function(req, res) {
-  const dateTime = new Date()
-  const itemInfo = req.body.item_information
-  const customerInfo = req.body.customer_information
+app.post("/send_order", function (req, res) {
+  const dateTime = new Date();
+  const itemInfo = req.body.item_information;
+  const customerInfo = req.body.customer_information;
   //turn serialized JSON into usable data
-  for (var i = 0; i < customerInfo.length; i++){
-    customerInfo[customerInfo[i]['name']] = customerInfo[i]['value'];
+  for (var i = 0; i < customerInfo.length; i++) {
+    customerInfo[customerInfo[i]["name"]] = customerInfo[i]["value"];
   }
-  console.log(customerInfo)
-  console.log(itemInfo)
-  send_order(dateTime,itemInfo,customerInfo)
-  update_quantities(itemInfo.id+'_'+itemInfo.color)
-})
+  console.log(customerInfo);
+  console.log(itemInfo);
+  send_order(dateTime, itemInfo, customerInfo);
+  update_quantities(itemInfo.id + "_" + itemInfo.color);
+});
 
-
-app.listen(PORT)
+app.listen(PORT);
